264ead6c45cba9318cea4fb49c9973f62c38fe3e
Matt T <mrsymtec@gmail.com>
2023-10-16 15:50:35 +1300

    Add project files.

---
+++ RPG-Game-Scenario2/main.cpp
@@ -0,0 +1,12 @@
+﻿// RPG-Game-Scenario2.cpp : Defines the entry point for the application.
+//
+
+#include "RPG-Game-Scenario2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ RPG-Game-Scenario2/player.h
@@ -0,0 +1,35 @@
+﻿// RPG-Game-Scenario2.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+enum Race { HUMAN, ELF, DWARF, ORC, TROLL };
+
+class Player {
+private:
+    std::string name;
+    Race race;
+    int hitPoints;
+    int magicPoints;
+
+public:
+    Player(std::string name, Race race, int hitPoints, int magicPoints);
+    virtual ~Player();
+
+    // Getters and setters
+    std::string getName() const;
+    Race getRace() const;
+    int getHitPoints() const;
+    int getMagicPoints() const;
+    void setName(std::string name);
+    void setRace(Race race);
+    void setHitPoints(int hitPoints);
+    void setMagicPoints(int magicPoints);
+
+    // Virtual attack function
+    virtual std::string attack() const;
+};
+
+// TODO: Reference additional headers your program requires here.
